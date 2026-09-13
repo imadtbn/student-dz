a728fd5459b4fce3de5f5cefe68eb09090c69e27
imad tbn <tabaniimadeddine@gmail.com>
2026-09-13 11:51:40 +0100

    Remove unused Web Push configuration

--- assets/js/push-config.js
+++
@@ -1,3 +0,0 @@
-// Set this to the HTTPS URL of your deployed Student DZ Web Push server.
-// Example: https://student-dz-push.example.com
-window.STUDENT_DZ_PUSH_API = 'https://YOUR-PUSH-SERVER.example.com';
